f08e6813989cc66a40aea107abb3095c486cfd59
Pochomo <xorkd119@gmail.com>
2025-02-24 21:13:16 +0900

    sp[documentation/#6] otlessì¶-check.ymlã ìlk 추가

--- resource-server/src/test/java/com/example/AppTest.java
+++ resource-server/src/test/java/com/example/AppTest.java
@@ -1,12 +1,20 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AppTest {
-    @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+	@Test
+	void contextLoads() {
+		// 기본적인 Spring Context 로드 테스트
+		assertTrue(true);
+	}
+
+	@Test
+	void mainClassExists() {
+		// Main 클래스 존재 여부 테스트
+		assertNotNull(new Main());
+	}
 }
